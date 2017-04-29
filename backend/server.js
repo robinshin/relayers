5ceb6ccf725a9c25a25a780f2cfbf3db92bebe01
Robin SHIN <robin.shin@telecom-paristech.fr>
2017-04-29 03:10:47 +0200

    found how to find version: added body-parser and mongodb dependencies

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
-//const bodyParser= require('body-parser')
+const bodyParser= require('body-parser')
 const app = express();
 
-//app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.use(express.static('frontend'));
 
