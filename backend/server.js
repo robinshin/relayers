d5b40cde582d810d264eb205139152dbaed8ab22
Robin SHIN <robin.shin@telecom-paristech.fr>
2017-04-29 03:27:41 +0200

    adapted server.js to new directories configuration

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const bodyParser= require('body-parser')
 const app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(bodyParser.urlencoded({extended: true}))
 
-app.use(express.static(__dirname));
+app.use(express.static('frontend'));
 
 app.post('/users', (req, res) => {
     db.collection('users').save(req.body, (err, result) => {
@@ -20,7 +22,7 @@ var db
 MongoClient.connect('mongodb://relayer:colislapepite@ds123331.mlab.com:23331/relayer-clients', (err, database) => {
     if (err) return console.log(err)
     db = database
-    app.listen(3000, () => {
-        console.log('listening on 3000')
+    app.listen(app.get('port'), () => {
+        console.log('Node app is running on port', app.get('port'));
     })
 })
