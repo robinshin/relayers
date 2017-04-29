4ecf071f095e67a2ee433d703442375568f82377
Robin SHIN <robin.shin@telecom-paristech.fr>
2017-04-29 03:22:07 +0200

    debug try

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser= require('body-parser')
 const app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.use(express.static('frontend'));
@@ -20,7 +22,7 @@ var db
 MongoClient.connect('mongodb://relayer:colislapepite@ds123331.mlab.com:23331/relayer-clients', (err, database) => {
     if (err) return console.log(err)
     db = database
-    app.listen(5000, () => {
-        console.log('listening on 5000')
+    app.listen(app.get('port'), () => {
+        console.log('Node app is running on port', app.get('port'));
     })
 })
