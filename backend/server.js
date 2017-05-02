27cd66156dc65587519bb28f4fc35e05694c98b5
Robin SHIN <robin.shin@telecom-paristech.fr>
2017-05-02 16:28:30 +0200

    added a nginx reverse-proxy server. I changed port to 8080

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser= require('body-parser')
 const app = express();
 
-app.set('port', 80);
+app.set('port', 8080);
 
 app.use(bodyParser.urlencoded({extended: true}))
 
