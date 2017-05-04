608fb5d9eaa583e324ae461bb10e8c07eca67096
Server <server@vps405229.ovh.net>
2017-05-04 14:14:27 +0200

    git bug

--- frontend/javascript/contact.js
+++ frontend/javascript/contact.js
@@ -36,29 +36,42 @@ function verifMsg(field)
     }
 }
 
-function verifForm(form)
-{
-    var mail = form.sender_mail;
-    var msg = form.msg;
-    var mailCorrect = verifMail(mail);
-    var msgCorrect = verifMsg(msg);
-    if (mailCorrect && msgCorrect)
-        return true;
-    else if (!mailCorrect)
-    {
-        highlight(mail, true);
-        alert("Veuillez rentrer une adresse e-mail correcte");
-        return false;
-    }
-    else if (!msgCorrect)
-    {
-        highlight(msg, true);
-        alert("Veuillez écrire un message");
-        return false;
-    }
-    else
-    {
-        alert("Veuillez remplir correctement tous les champs");
-        return false;
-    }
-}
+// JQuery
+$(function() {
+    $('#contact_btn').click(function() {
+        $.ajax({
+            url : '/contact',
+            type : 'POST',
+            dataType : 'json',
+            data : {
+            sender_mail : $('#sender_mail').val(),
+            msg : $('#msg').val(),
+            reponse : "null"}, // On fait passer nos variables, exactement comme en GET, au script more_com.php  
+            cache : false,
+            timeout : 5000,
+            complete : function() {
+                console.log("Done");
+            },
+
+            success: function(data) {
+                if (data.reponse == 'success') {
+                    document.getElementById("error-alert").style.display = 'none';
+                    document.getElementById("success-alert").style.display = 'block';
+                    document.getElementById("contact-form").reset();
+                }
+                else if (data.reponse == 'error') {
+                    document.getElementById("success-alert").style.display = 'none';
+                    document.getElementById("error-alert").style.display = 'block';
+                }
+                else {
+                    document.getElementById("success-alert").style.display = 'none';
+                    document.getElementById("error-alert").style.display = 'none';
+                }
+            },
+            error: function() {
+                document.getElementById("success-alert").style.display = 'none';
+                document.getElementById("error-alert").style.display = 'block';
+            }
+        });
+    });
+});
