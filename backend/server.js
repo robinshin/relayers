cb216c30941cae7f87a843d1d42d7e20bafd8cfa
Robin SHIN <robin.shin@telecom-paristech.fr>
2017-05-06 19:46:20 +0200

    new try

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ app.use(express.static('/home/server/relayers/frontend'));
 // Database
 var morgan = require('morgan');
 var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 
 mongoose.connect(process.env.MONGODB_URI + '/auth?authSource=admin');
 var db = mongoose.connection;
@@ -30,15 +31,60 @@ app.use(function(req, res, next) {
 
 // Passport
 var passport = require('passport');
-var api = require('./routes/api');
 
 app.use(passport.initialize());
 
-app.use('/api', require('./routes/api.js'));
 
 // Passport configuration
 var config = require('./config/database');
 
+//// Routes
+
+require('./config/passport')(passport);
+var jwt = require('jsonwebtoken');
+var User = require("./models/user");
+
+app.post('/register', function(req, res) {
+  if (!req.body.username || !req.body.password) {
+    res.json({success: false, msg: 'Please pass username and password.'});
+  } else {
+    var newUser = new User({
+      username: req.body.username,
+      password: req.body.password
+    });
+    // save the user
+    newUser.save(function(err) {
+      if (err) {
+        return res.json({success: false, msg: 'Username already exists.'});
+      }
+      res.json({success: true, msg: 'Successful created new user.'});
+    });
+  }
+});
+
+app.post('/login', function(req, res) {
+  User.findOne({
+    username: req.body.username
+  }, function(err, user) {
+    if (err) throw err;
+
+    if (!user) {
+      res.send({success: false, msg: 'Authentication failed. User not found.'});
+    } else {
+      // check if password matches
+      user.comparePassword(req.body.password, function (err, isMatch) {
+        if (isMatch && !err) {
+          // if user is found and password is right create a token
+          var token = jwt.sign(user, config.secret);
+          // return the information including token as JSON
+          res.json({success: true, token: 'JWT ' + token});
+        } else {
+          res.send({success: false, msg: 'Authentication failed. Wrong password.'});
+        }
+      });
+    }
+  });
+});
 
 //// Port
 app.listen(app.get('port'), () => {
@@ -46,3 +92,4 @@ app.listen(app.get('port'), () => {
 })
 
 module.exports = app;
+
