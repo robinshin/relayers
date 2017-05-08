664709ce176bf5ad2d396c204633c84265fdcda4
Robin SHIN <robin.shin@telecom-paristech.fr>
2017-05-08 22:53:15 +0200

    adding account validation json response

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,11 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static('/home/server/relayers/frontend/public'));
 app.use(express.static('/home/server/relayers/frontend'));
 
+app.get('/:account_validate', (req, res) => {
+    var account_validate = req.params.account_validate;
+    if (account_validate === 'true')
+        res.json({ reponse: 'account_validated' });
+});
 
 //// Port
 app.listen(app.get('port'), () => {
