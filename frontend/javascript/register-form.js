b816ee3acf397fb22649c0215ed053dc77d95492
Robin SHIN <robin.shin@telecom-paristech.fr>
2017-05-09 14:37:00 +0200

    preparing login response from server

--- frontend/javascript/register-form.js
+++ frontend/javascript/register-form.js
@@ -24,17 +24,17 @@ function checkEquals(field)
 function checkRegisterForm(form)
 {
     var firstName = form.firstName,
-        secondName = form.secondName,
-        mail = form.username,
-        address = form.address,
-        password = form.password,
-        password_confirm = form.password_confirm;
+    secondName = form.secondName,
+    mail = form.username,
+    address = form.address,
+    password = form.password,
+    password_confirm = form.password_confirm;
     
     var firstNameCorrect = checkLength(firstName),
-        secondNameCorrect = checkLength(secondName),
-        mailCorrect = checkMail(mail),
-        addressCorrect = checkLength(address),
-        passwordCorrect = checkEquals(password_confirm);
+    secondNameCorrect = checkLength(secondName),
+    mailCorrect = checkMail(mail),
+    addressCorrect = checkLength(address),
+    passwordCorrect = checkEquals(password_confirm);
     
     if (firstNameCorrect && secondNameCorrect && mailCorrect && passwordCorrect) {
         return true;
@@ -71,60 +71,61 @@ function checkRegisterForm(form)
     }
 }
 
-// JQuery
+//// JQuery
+// Register form
 $(function() {
-    $('#register_btn').click(function() {
-        if (!checkRegisterForm($("#register-form")[0]))
-            return;
-        $.ajax({
-            url : '/register',
-            type : 'POST',
-            dataType : 'json',
-            data : {
-                username : $('#username').val(),
-                password : $('#password').val(),
-                password_confirm : $('#password_confirm').val(),
-                firstName : $('#firstName').val(),
-                secondName : $('#secondName').val(),
-                address : $('#autocomplete').val()
-            },
-            cache : false,
-            timeout : 5000,
-
-            success: function(data) {
-                if (data.reponse == 'success') {
-                    display_register_alert(true, 'success');
-                    $("#register-form")[0].reset();
-                }
-                else {
-                    if (data.msg == 'empty fields') {
-                        display_register_alert(false, 'empty_fields');
-                    }
-                    else if (data.msg == 'wrong username') {
-                        display_register_alert(false, 'wrong_username');
-                    }
-                    else if (data.msg == 'passwords mismatch') {
-                        display_register_alert(false, 'passwords_mismatch');
-                    }
-                    else if (data.msg == 'already registered') {
-                        display_register_alert(false, 'already_registered');
-                    }
-                    else if (data.msg == 'verif mail already sent') {
-                        display_register_alert(false, 'verif_mail_already_sent');
-                    }
-                    else {
-                        display_register_alert(false, 'unknown');
-                    }
-                    checkRegisterForm($('#register-form')[0]);
-               }
-            },
-
-            error: function() {
-                display_register_alert(false, 'unknown');
-            }
-        });
-    });
-});
+  $('#register_btn').click(function() {
+                           if (!checkRegisterForm($("#register-form")[0]))
+                           return;
+                           $.ajax({
+                                  url : '/register',
+                                  type : 'POST',
+                                  dataType : 'json',
+                                  data : {
+                                  username : $('#username').val(),
+                                  password : $('#password').val(),
+                                  password_confirm : $('#password_confirm').val(),
+                                  firstName : $('#firstName').val(),
+                                  secondName : $('#secondName').val(),
+                                  address : $('#autocomplete').val()
+                                  },
+                                  cache : false,
+                                  timeout : 5000,
+                                  
+                                  success: function(data) {
+                                  if (data.reponse == 'success') {
+                                  display_register_alert(true, 'success');
+                                  $("#register-form")[0].reset();
+                                  }
+                                  else {
+                                  if (data.msg == 'empty fields') {
+                                  display_register_alert(false, 'empty_fields');
+                                  }
+                                  else if (data.msg == 'wrong username') {
+                                  display_register_alert(false, 'wrong_username');
+                                  }
+                                  else if (data.msg == 'passwords mismatch') {
+                                  display_register_alert(false, 'passwords_mismatch');
+                                  }
+                                  else if (data.msg == 'already registered') {
+                                  display_register_alert(false, 'already_registered');
+                                  }
+                                  else if (data.msg == 'verif mail already sent') {
+                                  display_register_alert(false, 'verif_mail_already_sent');
+                                  }
+                                  else {
+                                  display_register_alert(false, 'unknown');
+                                  }
+                                  checkRegisterForm($('#register-form')[0]);
+                                  }
+                                  },
+                                  
+                                  error: function() {
+                                  display_register_alert(false, 'unknown');
+                                  }
+                                  });
+                           });
+  });
 
 function display_register_alert(success, msg_id) {
     if (success) {
@@ -136,10 +137,90 @@ function display_register_alert(success, msg_id) {
         $('#register-alert').addClass('alert-danger');
     }
     var msg;
-	switch (msg_id) {
+    switch (msg_id) {
         case 'success':
             msg = '<strong>Inscription validée !</strong> Un mail de confirmation vous a été envoyé !'
             break;
+        case 'empty_fields':
+            msg = '<strong>Veuillez renseigner tous les champs</strong>';
+            break;
+        case 'wrong_username':
+            msg = '<strong>Veuillez vérifier votre adresse e-mail</strong>';
+            break;
+        case 'passwords_mismatch':
+            msg = '<strong>Les mots de passe ne correspondent pas</strong>';
+            break;
+        case 'already_registered':
+            msg = '<strong>Vous êtes déjà inscrit</strong>';
+            break;
+        case 'verif_mail_already_sent':
+            msg = '<strong>Vous devez valider votre compte</strong>'
+            break;
+        default:
+            msg = '<strong>Erreur inconnue</strong>'
+    }
+    $('#text-register-alert').html(msg);
+    $('#register-alert').css('display', 'block');
+}
+
+
+// Login form
+$(function() {
+  $('#login_btn').click(function() {
+                        //if (!checkRegisterForm($("#register-form")[0]))
+                        //return;
+                        $.ajax({
+                               url : '/login',
+                               type : 'POST',
+                               dataType : 'json',
+                               data : {
+                               username : $('#login_username').val(),
+                               password : $('#login_password').val()
+                               },
+                               cache : false,
+                               timeout : 5000,
+                               
+                               /*success: function(data) {
+                                if (data.reponse == 'success') {
+                                display_register_alert(true, 'success');
+                                $("#register-form")[0].reset();
+                                }
+                                else {
+                                if (data.msg == 'empty fields') {
+                                display_register_alert(false, 'empty_fields');
+                                }
+                                else if (data.msg == 'wrong username') {
+                                display_register_alert(false, 'wrong_username');
+                                }
+                                else if (data.msg == 'passwords mismatch') {
+                                display_register_alert(false, 'passwords_mismatch');
+                                }
+                                else if (data.msg == 'already registered') {
+                                display_register_alert(false, 'already_registered');
+                                }
+                                else if (data.msg == 'verif mail already sent') {
+                                display_register_alert(false, 'verif_mail_already_sent');
+                                }
+                                else {
+                                display_register_alert(false, 'unknown');
+                                }
+                                checkRegisterForm($('#register-form')[0]);
+                                }
+                                },
+                                
+                                error: function() {
+                                display_register_alert(false, 'unknown');
+                                }*/
+                               });
+                        });
+  });
+
+function display_login_alert(success, msg_id) {
+    if (success) {
+        return ;
+    }
+    var msg;
+    switch (msg_id) {
         case 'empty_fields':
             msg = '<strong>Veuillez renseigner tous les champs</strong>';
             break;
