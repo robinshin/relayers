00e0f11d4101a419a3e0af712c65e39a0b3e25e1
Robin SHIN <robin.shin@telecom-paristech.fr>
2017-05-09 16:11:24 +0200

    cleaning

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser= require('body-parser')
+const app = express();
+
+app.set('port', 8080);
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(express.static('/home/server/relayers/frontend/public'));
+app.use(express.static('/home/server/relayers/frontend'));
+
+//// Port
+app.listen(app.get('port'), () => {
+    console.log('Node app is running on port', app.get('port'));
+})
+
+module.exports = app;
+
