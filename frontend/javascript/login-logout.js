f106b0cb07fa90d0af28aa9272165ee863ee400e
Robin SHIN <robin.shin@telecom-paristech.fr>
2017-05-14 17:50:33 +0200

    forgot a file

---
+++ frontend/javascript/login-logout.js
@@ -0,0 +1,45 @@
+function readCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+    var c = ca[i];
+    while (c.charAt(0)==' ') c = c.substring(1,c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
+
+
+//// JQuery
+// Checks if user is logged in
+$(function() {
+  var token = readCookie('token');
+  if (token) {
+    $.ajax({
+      url :"/auth",
+      type:'POST',
+      headers : { "Authorization" : token },
+      dataType: 'json',
+      data: {
+        role: data.role
+      },
+      cache: false,
+      timeout: 5000,
+
+      success: function(data) {
+        if (data.reponse === "success") {
+          $('#profile').css('display', 'block');
+          $('#login').css('display', 'none');
+        }
+        else {
+          $('#profile').css('display', 'none');
+          $('#login').css('display', 'none');
+        }
+      }
+    });
+  }
+  else {
+    $('#profile').css('display', 'none');
+    $('#login').css('display', 'none');
+  }
+});
