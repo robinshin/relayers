ab4aebb555e6c3701f278913511cfe092aed0605
Robin SHIN <robin.shin@telecom-paristech.fr>
2017-05-15 23:50:46 +0200

    trying pug

--- backend/server.js
+++ backend/server.js
@@ -2,25 +2,32 @@ const express = require('express');
 const bodyParser= require('body-parser')
 const app = express();
 
-app.set('port', 8080);
+app.use(bodyParser.urlencoded({extended: true}))
 
+//// Configure pug
 app.set("view engine", "pug");
 app.set("views", '/home/server/relayers/frontend/views');
 
-app.use(bodyParser.urlencoded({extended: true}))
 
 //// Protected page
 app.use('/profile.html', (req, res) => {
   res.redirect("https://relayers.fr/profile");
-})
+});
 
 app.get('/', (req, res) => {
-  res.render("index");
+  if checkAuthentication(req) {
+    res.render('index', { logged: true });
+  }
+  else {
+    res.render('index', {logged: false});
+  }
+
 });
 
 app.use(express.static('/home/server/relayers/frontend/public'));
 
 //// Port
+app.set('port', 8080);
 app.listen(app.get('port'), () => {
   console.log('Node app is running on port', app.get('port'));
 })
