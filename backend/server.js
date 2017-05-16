7fb3440441d13d55677009e30e8b58f503afabcb
Robin SHIN <robin.shin@telecom-paristech.fr>
2017-05-16 15:10:39 +0200

    debug

--- backend/server.js
+++ backend/server.js
@@ -193,13 +193,13 @@ app.get('/email-verification/:URL', (req, res) => {
     if (user) {
       nev.sendConfirmationEmail(user.username, function(err, info) {
         if (err) {
-          return res.render('index', { mailConfirmed: true,  text-mail-confirmedModal: 'Erreur : le lien de confirmation est incorrect', glyphicon-mail-confirmedModal: 'glyphicon-remove'});
+          return res.render('index', { mailConfirmed: true,  text-mail-confirmedModal: 'Erreur : le lien de confirmation est incorrect', glyphiconmailconfirmedModal: 'glyphicon-remove'});
         }
         res.redirect('https://relayers.fr/' + '?confirm=true');
       });
     }
     else {
-      return res.render('index', { mailConfirmed: true,  text-mail-confirmedModal: 'Erreur : le lien de confirmation est incorrect', glyphicon-mail-confirmedModal: glyphicon-remove});
+      return res.render('index', { mailConfirmed: true,  text-mail-confirmedModal: 'Erreur : le lien de confirmation est incorrect', glyphiconmailconfirmedModal: 'glyphicon-remove'});
     }
   });
 });
